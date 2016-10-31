241398bd62d483329e6de373af4ef76f6f1a24f5
Lulu Atikah <luluatikah01@gmail.com>
2016-10-31 13:13:21 +0700

    change

--- report.py
+++ report.py
@@ -1,1 +1,30 @@
-from openerp import osv, api, models
+from openerp import osv, api, models, fields
+from openerp import tools
+
+class report(osv.osv):
+    _name = "graph.report"
+    _auto = False
+
+    _columns = {
+        'tanggal' : fields.date('Tanggal', readonly=True),
+        'ashar' : fields. many2one('mutabaah.ashar', 'Ashar', readonly=True),
+        'maghrib' : fields.many2one('mutabaah.maghrib', 'Maghrib', readonly=True),
+        'isya' : fields.many2one('mutabaah.isya', 'Isya', readonly=True),
+        'tilawah' : fields.many2one('mutabaah.tilawah', 'Tilawah', readonly=True),
+    }
+
+    def init(self, cr):
+        tools.drop_view_if_exists(cr, 'graph.report')
+        cr.execute("""
+            CREATE OR REPLACE VIEW graph.report AS (
+            SELECT
+                m.tanggal,
+                m.ashar,
+                m.maghrib,
+                m.isya,
+                m.tilawah
+                FROM
+                "mutabaah" m
+
+            ) """
+        )
