c38a87998e1d164e2cadc9f65220a57a03ecc71b
Lulu Atikah <luluatikah01@gmail.com>
2016-11-23 13:12:21 +0700

    new

--- __openerp__.py
+++ __openerp__.py
@@ -3,20 +3,19 @@
     'name': "mutabaah",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+       """,
 
     'description': """
-        Long description of module's purpose
+        This modul aims to manage mutabaah yaumiyah of santri karya in DPU DT
     """,
 
-    'author': "Your Company",
+    'author': "L. Atikah",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Human Resources',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -27,7 +26,7 @@
         # 'security/ir.model.access.csv',
         'templates.xml',
         'views/mutabaah.xml',
-        #'views/report.xml',
+        #'report.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
